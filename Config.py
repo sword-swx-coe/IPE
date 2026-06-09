9f8efbd0a8e96fda8d016e505ae534e5d7010f54
Aaron Ridley <ridley@umich.edu>
2026-06-09 18:05:58 -0400

    works now

--- Config.py
+++ Config.py
@@ -3,7 +3,6 @@
 import os
 import argparse
 
-
 IsVerbose = False
 
 # ----------------------------------------------------------------------
@@ -17,6 +16,10 @@ def parse_args():
     parser.add_argument('-name', \
                         help = 'job name', \
                         default = 'boring_name')
+
+    parser.add_argument('-v',  \
+                        action='store_true', default = False, \
+                        help = 'set verbose to true')
     
     args = parser.parse_args()
 
@@ -39,11 +42,13 @@ def run_command(command):
 
 def get_electrodynamics(ieDir = "ext/Electrodynamics"):
 
+    didWork = True
+    
     # Check if the above ran successfully
     if (not os.path.exists(ieDir)):
 
         command = \
-            "git clone git\@github.com:GITMCode/Electrodynamics.git " + ieDir
+            "git clone git@github.com:GITMCode/Electrodynamics.git " + ieDir
         if (IsVerbose):
             print("-> Attempting to clone GITMCode/Electrodynamics with ssh.")
         run_command(command)
@@ -64,26 +69,131 @@ def get_electrodynamics(ieDir = "ext/Electrodynamics"):
                 print("-> Electrodynamics (https) was cloned successfully!")
             else:
                 print("-> git clone with HTTPS failed. Giving up...")
-                exit();
+                return False
     else:
         print(" --> " + ieDir + " already exists.")
         command = "git -C " + ieDir + " pull"
         run_command(command)
     command = "touch " + ieDir + "/src/Makefile.DEPEND";
-    if (isVerbose):
+    if (IsVerbose):
         print(" --> creating DEPEND file in electrodynamics:")
-    run_command($command)
+    run_command(command)
 
-    return
+    return didWork
+
+# ----------------------------------------------------------------------
+# clone indices library (or update it)
+# ----------------------------------------------------------------------
+
+def get_indices(ioDir = "ext/srcIndices"):
+
+    didWork = True
+    
+    # Check if the above ran successfully
+    if (not os.path.exists(ioDir)):
+
+        command = \
+            "git clone git@github.com:GITMCode/srcIndices.git " + ieDir
+        if (IsVerbose):
+            print("-> Attempting to clone GITMCode/srcIndices with ssh.")
+        run_command(command)
+
+        # If this fails, the script will *not* crash.
+        #   therefore, need to check if it was successful:
+        if (os.path.exists(ioDir)):
+            print("-> srcIndices was cloned successfully!")
+        else:
+            
+            print("-> git clone with SSH failed. Trying HTTPS...")
+            command = \
+                "git clone https://github.com/GITMCode/srcIndices.git " \
+                + ioDir
+            run_command(command)
+            # If this fails, the script will exit with an error message
+            if (os.path.exists(ioDir)):
+                print("-> srcIndices (https) was cloned successfully!")
+            else:
+                print("-> git clone with HTTPS failed. Giving up...")
+                return False
+    else:
+        print(" --> " + ioDir + " already exists.")
+        command = "git -C " + ioDir + " pull"
+        run_command(command)
+    command = "touch " + ioDir + "/src/Makefile.DEPEND";
+    if (IsVerbose):
+        print(" --> creating DEPEND file in srcIndices/src:")
+    run_command(command)
+
+    return didWork
 
 # ----------------------------------------------------------------------
 # 
 # ----------------------------------------------------------------------
 
+def make_makefile_dirs(mainDir):
 
+    baselineDirsFile = mainDir + '/build/Makefile.dirs.base'
+    dirsFile = mainDir + '/build/Makefile.dirs'
+    baseDirString = 'BASEDIR = ' + mainDir
+    
+    if (IsVerbose):
+        print(' -> Reading file : ', baselineDirsFile)
+    fpin = open(baselineDirsFile, 'r')
+    allLines = fpin.readlines()
+    fpin.close()
+
+    command = 'rm -f ' + dirsFile
+    run_command(command)
+    if (IsVerbose):
+        print(' -> Writing file : ', dirsFile)
+    fpout = open(dirsFile, 'w')
+    fpout.write('\n')
+    fpout.write(baseDirString + '\n')
+    fpout.write('\n')
+    for line in allLines:
+        fpout.write(line)
+    fpout.close()
+    return dirsFile
+    
 # ----------------------------------------------------------------------
 # main code:
 # ----------------------------------------------------------------------
 
 args = parse_args()
+if (args.v):
+    IsVerbose = True
+
+mainDir = os.getcwd()
+
+makefileDirs = make_makefile_dirs(mainDir)
+
+mileDir = 'ext/Electrodynamics'
+
+didWork = get_electrodynamics(ieDir = mileDir)
+if (not didWork):
+    print('Downloading electrodynamics library failed!')
+    exit()
+
+command = \
+    'cd ' + mileDir + '/build ; ' + \
+    'rm -f Makefile.dirs ; ' + \
+    'ln -s ' + makefileDirs + ' .'
+run_command(command)
+
+ioDir = "ext/srcIndices"
+didWork = get_indices(ioDir = ioDir)
+if (not didWork):
+    print('Downloading indices library failed!')
+    exit()
+command = \
+    'cd ' + ioDir + '/build ; ' + \
+    'rm -f Makefile.dirs ; ' + \
+    'ln -s ' + makefileDirs + ' .'
+run_command(command)
+
+command = 'rm -f Makefile'
+run_command(command)
+
+command = 'ln -s build/Makefile.um.maindir ./Makefile'
+run_command(command)
 
