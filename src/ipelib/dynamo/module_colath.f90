e0bae4d84a18db47f8de77d9648e764b9e1f01a9
Aaron Ridley <ridley@umich.edu>
2026-07-23 11:36:25 -0400

    tiny cleaning

--- src/ipelib/dynamo/module_colath.f90
+++ src/ipelib/dynamo/module_colath.f90
@@ -1,15 +1,16 @@
 !
-      module module_colath
+module module_colath
 !
-      PRIVATE
-      PUBLIC :: colath
-      contains 
+  PRIVATE
+  PUBLIC :: colath
+
+  contains 
 !-----------------------------------------------------------------------
-      subroutine colath(offset1_deg,offset2_deg)
-!
-! Calculate pfrac fractional presence of dynamo equation using critical
-!  convection colatitudes crit(2).  (crit is in cons module)
-!
+  subroutine colath(offset1_deg,offset2_deg)
+    !
+    ! Calculate pfrac fractional presence of dynamo equation using critical
+    !  convection colatitudes crit(2).  (crit is in cons module)
+    !
       use heelis_module !,ONLY:what?
       use module_magfield,only: sunlons 
       use cons_module,only: rtd,                                        &
@@ -28,6 +29,8 @@
       real :: sinlat,coslat,aslonc,ofdc,cosofc,sinofc
 !
 ! offc(2), dskofc(2) are for northern hemisphere aurora (see aurora.F)
+
+      !write(*,*) 'colath : ', offc(2), dskofc(2), kmlat0, theta0
       ofdc = sqrt(offc(2)**2+dskofc(2)**2)
       cosofc = cos(ofdc)
       sinofc = sin(ofdc)
@@ -44,20 +47,19 @@
           colatc(i,j) = acos(cosofc*sinlat-sinofc*coslat*colatc(i,j))
         enddo ! i=1,kmlonp1
 
-!
-! Calculate fractional presence of dynamo equation at each northern
-! hemisphere geomagnetic grid point. Output in pfrac(kmlonp1,kmlat0)
-!
-         crit(1)=theta0(1)+(offset1_deg*pi_dyn/180.)
-         crit(2)=theta0(1)+(offset2_deg*pi_dyn/180.)
-	do i=1,kmlonp1
+        !
+        ! Calculate fractional presence of dynamo equation at each northern
+        ! hemisphere geomagnetic grid point. Output in pfrac(kmlonp1,kmlat0)
+        !
+        crit(1)=theta0(1)+(offset1_deg*pi_dyn/180.)
+        crit(2)=theta0(1)+(offset2_deg*pi_dyn/180.)
+	  do i=1,kmlonp1
           pfrac(i,j) = (colatc(i,j)-crit(1))/(crit(2)-crit(1))
           if (pfrac(i,j) < 0.) pfrac(i,j) = 0.
           if (pfrac(i,j) >= 1.) pfrac(i,j) = 1.
-	enddo ! i=1,kmlonp1
+	  enddo ! i=1,kmlonp1
 
-!       write(6,"('colath: j=',i3,' pfrac(:,j)=',/,(6e12.4))")
-!     &    j,pfrac(:,j)
+        !write(6,"('colath: j=',i3,' pfrac(:,j)=',/,(6e12.4))") j,pfrac(:,j)
 
       enddo ! j=1,kmlat0
 
